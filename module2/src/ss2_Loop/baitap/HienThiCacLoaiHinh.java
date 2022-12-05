783db02b8240f6d04073ea1394bb7c0bad03aef5
vandong97 <phanvandong19977@gmail.com>
2022-12-05 21:50:40 +0700

    nop bt

--- module2/src/ss2_Loop/baitap/HienThiCacLoaiHinh.java
+++ module2/src/ss2_Loop/baitap/HienThiCacLoaiHinh.java
@@ -4,10 +4,65 @@ import java.util.Scanner;
 
 public class HienThiCacLoaiHinh {
     public static void main(String[] args) {
-        int chonHinh = -1;
-        Scanner scanner = new Scanner(System.in);// nhập dữ liệu là phải có dòng này
-        while (chonHinh != 4){
-
+        int choice;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("1.Print the rectangle");
+            System.out.println("2.Print the square triangle ");
+            System.out.println("3.Print isosceles triangle");
+            System.out.println("4.Exit");
+            System.out.println("Enter your choice");
+            choice = Integer.parseInt(scanner.nextLine());
+            switch (choice) {
+                case 1:
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 7; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j <= i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println();
+                    for (int i = 5; i <= 1; i--) {
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println();
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int k = 5; k > i; k--) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i <= 5; i++) {
+                        for (int j = 10; j > i; j--) {
+                            System.out.print(" ");
+                        }
+                        for (int k = 1; k <= i * 2 - 1; k++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 4:
+                    System.exit(4);
+                default:
+                    System.out.println("No choice!");
+            }
         }
     }
 }
