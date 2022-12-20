b74cbc2b25c841af3e528babd5f5365eac3d1833
vandong97 <phanvandong19977@gmail.com>
2022-12-20 09:26:56 +0700

    nop bt

--- module2/src/ss13_Thuat_toan_tim_kiem/thuc_hanh/AlgorithmComplexityTest.java
+++ module2/src/ss13_Thuat_toan_tim_kiem/thuc_hanh/AlgorithmComplexityTest.java
@@ -9,17 +9,17 @@ public class AlgorithmComplexityTest {
         String string = scanner.nextLine();
 
         int frequentChar[] = new int[255];
-        for (int i = 0; i < frequentChar.length; i++) {
+        for (int i = 0; i < string.length(); i++) {
             int ascii = (int) string.charAt(i);
             frequentChar[ascii] += 1;
         }
 
         int max = 0;
         char character = (char) 255;
-        for (int i = 0; i < 255; i++) {
-            if (frequentChar[i] > max) {
-                max = frequentChar[i];
-                character = (char) i;
+        for (int j = 0; j < 255; j++) {
+            if (frequentChar[j] > max) {
+                max = frequentChar[j];
+                character = (char) j;
             }
         }
         System.out.println("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
